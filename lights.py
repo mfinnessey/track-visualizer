bfd915ba4f44a03bf3406cc85227cedc09b62e58
Michael Finnessey <mrfinnessey@gmail.com>
2022-12-14 21:09:22 -0800

    add bpm test

--- lights.py
+++ lights.py
@@ -3,6 +3,25 @@
 import os
 import errno
 import time
+from rpi_ws281x import PixelStrip, Color
+
+LED_COUNT = 50
+LED_PIN = 18 # GPIO pins are not necessarily the same as physical pins
+LED_FREQ_HZ = 800000
+LED_DMA = 10
+LED_BRIGHTNESS = 255
+LED_INVERT = False
+LED_CHANNEL = 0
+
+def bpm_pulse(strip, color, bpm):
+    beat_length_ms = 60000 / bpm
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+    strip.show()
+    while True:
+        strip.setBrightness(0)
+        time.sleep(beat_length_ms)
+        strip.setBrightness(255)
 
 if __name__ == "__main__":
     print("starting!")
@@ -18,6 +37,12 @@ if __name__ == "__main__":
             print("pipe exists, things are fine")
 
     pipe = os.open(pipe_name, os.O_RDONLY | os.O_NONBLOCK)
+
+    # begin strip operations
+    strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    strip.begin()
+    bpm_pulse(strip, Color(0, 0, 255), 120) # blue @ 120 bpm
+
     print("Ready to process messages!")
     # process messages
     try:
