72369307a19c6c08ae18d67a3df8d26acb1c1584
Michael Finnessey <mrfinnessey@gmail.com>
2022-12-15 13:03:48 -0800

    implement light control end of messaging protocol
    implemented with multithreading to be just a hair cleaner in the long run
    main thread checks named pipe for and processes messages
    light control thread produces the requested effects as processed by the main
    thread
    thread communication takes place via shared state
    locking is only needed for notification of a new message (all other pieces of
    shared state are only written by the maint thread)

--- lights.py
+++ lights.py
@@ -3,8 +3,18 @@
 import os
 import errno
 import time
+import threading
 from rpi_ws281x import PixelStrip, Color
 
+# globals (for shared state between threads)
+# shared state to notify light control thread of a newly arrived msg
+new_msg = True
+new_msg_lock = threading.Lock()
+# results of parsing a message
+data = None
+
+
+# light constants
 LED_COUNT = 50
 LED_PIN = 18 # GPIO pins are not necessarily the same as physical pins
 LED_FREQ_HZ = 800000
@@ -13,11 +23,19 @@ LED_BRIGHTNESS = 255
 LED_INVERT = False
 LED_CHANNEL = 0
 
+# communication constants
+
+# messages are padded to this length to prevent concatenation of multiple messages
+MAX_MSG_LENGTH = 100
+
 def bpm_pulse(strip, color, bpm):
     beat_length = 60.0 / bpm
     for i in range(strip.numPixels()):
         strip.setPixelColor(i, color)
     while True:
+        # end if new message arrived
+        if new_msg:
+            return
         strip.setBrightness(0)
         strip.show()
         time.sleep(beat_length)
@@ -25,38 +43,75 @@ def bpm_pulse(strip, color, bpm):
         strip.show()
         time.sleep(beat_length)
 
+def color_from_list(list):
+    return Color(int(list[0]), int(list[1]), int(list[2]))
+
+def parse_msg(msg):
+    # no checking for malformed messages as sensible recovery is likely impossible
+
+    # format of msg is effect|color1;(color2)|bpm
+    # colors are specified as RRR,GGG,BBB
+    components = msg.split('|')
+    # process colors
+    components[1] = components[1].split(';')
+    colors_lists = [color.split(',') for color in components[1]]
+    components[1] = [color_from_list(color) for color in colors_lists]
+    components[2] = int(components[2])
+    return components
+
+
+def light_control_thread(strip):
+    global new_msg
+    while True:
+        # acknowledge new msg
+        new_msg_lock.acquire()
+        new_msg = False
+        new_msg_lock.release()
+
+        effect = data[0]
+        colors = data[1]
+        bpm = data[2]
+
+        # carry out requested effect
+        if effect == "two_color_cycle":
+            two_color_cycle(strip, colors[0], colors[1], bpm)
+        else: # default to bpm pulsing
+            bpm_pulse(strip, colors[0], bpm)
+
+
 if __name__ == "__main__":
-    print("starting!")
     pipe_name = "./track-visualizer"
     try:
         os.mkfifo(pipe_name)
     except OSError as e:
-        print("error")
         # don't raise error for already existing pipe
         if e.errno != errno.EEXIST:
             raise e
-        else:
-            print("pipe exists, things are fine")
 
     pipe = os.open(pipe_name, os.O_RDONLY | os.O_NONBLOCK)
 
     # begin strip operations
     strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
     strip.begin()
-    bpm_pulse(strip, Color(0, 0, 255), 120) # blue @ 120 bpm
 
-    print("Ready to process messages!")
     # process messages
     try:
+        # spawn light control thread, defaulting to bpm pulsing blue @ 120
+        msg = "bpm_pulse|000,000,255|120"
+        data = parse_msg(msg)
+        lights = threading.Thread(target = light_control_thread(strip))
         while True:
-            # read at most 24 bytes in a message
-            msg = os.read(pipe, 24)
+            msg = os.read(pipe, MAX_MSG_LENGTH)
             # wait 1 second after an unsuccessful read attempt
             if not msg:
                 time.sleep(1)
             else:
                 print("Read msg: " + str(msg))
+                data = parse_msg(msg)
+                # notify light control thread of new message
+                new_msg_lock.acquire()
+                new_msg = True
+                new_msg_lock.release()
     finally:
         os.close(pipe)
         os.remove(pipe_name)
-
