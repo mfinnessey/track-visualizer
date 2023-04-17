e0defc8cc27c739fbfcc6c7a49db6bb0323e2f71
Michael Finnessey <mrfinnessey@gmail.com>
2023-04-17 00:02:46 -0700

    increase server robustness
    attempt to handle a lot of common usability issues
    - state mismatch
    - not logged in
    
    also improve commenting and indentation

--- express/routes/control.js
+++ express/routes/control.js
@@ -12,9 +12,13 @@ const pipe = fs.createWriteStream(pipe_name);
 
 router.get('/', function(req, res) {
 
+    // if not logged in, force the user to log in as otherwise API calls will necessarily fail
+    if(!req.cookies) res.redirect('login');
+
     var access_token = req.cookies["access_token"];
 
-    var effect = req.query.effect;
+    // vaildate inputs and abort message creation if invalid
+    var effect = req.query.effect; // should never be invalid as dropdown
     var r0 = req.query.r0;
     var g0 = req.query.g0;
     var b0 = req.query.b0;
@@ -22,21 +26,24 @@ router.get('/', function(req, res) {
     var g1 = req.query.g1;
     var b1 = req.query.b1;
 
-    // get currently playing song information if other information was provided
-    if(r0 == null) {
-        res.render('control');
-        return;
+    // a primary color must always be specified
+    if(r0 == null || g0 == null || b0 == null) res.redirect('control');
+
+    // some effects require a secondary color to be specified
+    if(r1 == null || g1 == null || b1 == null){
+        if(effect == "two_color_cycle") res.redirect('control');
     }
+
+    // get currently playing song information to sync lights to tempo
     var reqInfo = {
         url: 'https://api.spotify.com/v1/me/player/currently-playing',
         headers: {'Authorization': 'Bearer ' + access_token},
     };
 
     request.get(reqInfo, function(error, response, body) {
-        // successful request
         if (!error && response.statusCode === 200) {
             var current_id = JSON.parse(body).item.id;
-            // get audio analysis of currently playing song
+            // get audio analysis of currently playing song for tempo
             reqInfo = {
                 url: 'https://api.spotify.com/v1/audio-features?ids=' + current_id,
                 headers: {'Authorization': 'Bearer ' + access_token}
@@ -55,19 +62,15 @@ router.get('/', function(req, res) {
                     pipe.write(msg);
 
                 }
+                // in either failing case, access token has become invalid
+                // therefore, we should get a new access token using the refresh token
                 else{
-                    res.redirect('/#' +
-                                 querystring.stringify({
-                                     error: 'invalid_token'
-                                 }));
+                    res.redirect('refresh_token');
                 }
             });
 
         } else {
-            res.redirect('/#' +
-                         querystring.stringify({
-                             error: 'invalid_token'
-                         }));
+            res.redirect('refresh_token');
         }
     });
 
