a9d1676afbf5631fb912de6ef25d8b39af3f2baf
Michael Finnessey <mrfinnessey@gmail.com>
2023-04-17 18:22:45 -0700

    clean up Python code
    follow the linter mostly
    
    some public / private decisions are probably slightly nonsensical

--- lights.py
+++ lights.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-
+"""Control ws2812 LEDs from RPi based off of incoming control messages."""
 import os
 import errno
 import time
@@ -28,11 +28,13 @@ LED_CHANNEL = 0
 
 # communication constants
 
-# messages are padded to this length to prevent concatenation of multiple messages
+# messages are padded to this length to prevent concatenation of multiple
+# messages
 MAX_MSG_LENGTH = 100
 
 
 def snake(strip, color, bpm):
+    """Create a snake effect on LED strips."""
     beat_length = 60.0 / bpm
     strip.setBrightness(255)
     on = 0
@@ -51,6 +53,7 @@ def snake(strip, color, bpm):
 
 
 def two_color_cycle(strip, color_1, color_2, bpm):
+    """Alternate between two colors on LED strips."""
     beat_length = 60.0 / bpm
     strip.setBrightness(255)
     while True:
@@ -66,7 +69,9 @@ def two_color_cycle(strip, color_1, color_2, bpm):
         strip.show()
         time.sleep(beat_length)
 
+
 def bpm_pulse(strip, color, bpm):
+    """Pulse LED strip on and off on a singular color."""
     beat_length = 60.0 / bpm
     for i in range(strip.numPixels()):
         strip.setPixelColor(i, color)
@@ -81,11 +86,14 @@ def bpm_pulse(strip, color, bpm):
         strip.show()
         time.sleep(beat_length)
 
-def color_from_list(list):
+
+def __color_from_list(list):
     return Color(int(list[0]), int(list[1]), int(list[2]))
 
-def parse_msg(msg):
-    # no checking for malformed messages as sensible recovery is likely impossible
+
+def __parse_msg(msg):
+    # no checking for malformed messages as sensible recovery
+    # is likely impossible
 
     # format of msg is effect|color1;(color2)|bpm
     # colors are specified as RRR,GGG,BBB
@@ -93,12 +101,12 @@ def parse_msg(msg):
     # process colors
     components[1] = components[1].split(';')
     colors_lists = [color.split(',') for color in components[1]]
-    components[1] = [color_from_list(color) for color in colors_lists]
+    components[1] = [__color_from_list(color) for color in colors_lists]
     components[2] = int(components[2])
     return components
 
 
-def light_control_thread(strip):
+def __light_control_thread(strip):
     global new_msg
     while True:
         # acknowledge new msg
@@ -115,7 +123,8 @@ def light_control_thread(strip):
             two_color_cycle(strip, colors[0], colors[1], bpm)
         elif effect == "snake":
             snake(strip, colors[0], bpm)
-        else: # default to bpm pulsing
+        # default to bpm pulsing
+        else:
             bpm_pulse(strip, colors[0], bpm)
 
 
@@ -131,15 +140,16 @@ if __name__ == "__main__":
     pipe = os.open(pipe_name, os.O_RDONLY | os.O_NONBLOCK)
 
     # begin strip operations
-    strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT,
+                       LED_BRIGHTNESS, LED_CHANNEL)
     strip.begin()
 
     # process messages
     try:
         # spawn light control thread, defaulting to bpm pulsing blue @ 120
         msg = "bpm_pulse|000,000,255|120"
-        data = parse_msg(msg)
-        lights = threading.Thread(target = light_control_thread, args=(strip,))
+        data = __parse_msg(msg)
+        lights = threading.Thread(target=__light_control_thread, args=(strip,))
         lights.start()
         while True:
             # retry to avoid weird edge case around node startup
@@ -154,7 +164,7 @@ if __name__ == "__main__":
             if not msg:
                 time.sleep(1)
             else:
-                data = parse_msg(str(msg)[2:-1])
+                data = __parse_msg(str(msg)[2:-1])
                 # notify light control thread of new message
                 new_msg_lock.acquire()
                 new_msg = True
