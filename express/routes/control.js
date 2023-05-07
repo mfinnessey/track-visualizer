3343064bba45382ff4bfb8974add1413080ad2e5
Michael Finnessey <mrfinnessey@gmail.com>
2023-05-07 01:09:18 -0700

    I love JS

--- express/routes/control.js
+++ express/routes/control.js
@@ -12,7 +12,7 @@ const pipe = fs.createWriteStream(pipe_name);
 
 function forceValidColor(val){
     // force colors values to be in range 0 to 255
-    if(Number.isInteger(val) && val >= 0 && val <= 255) return val;
+    if(val != null && Number.isInteger(Number(val)) && val >= 0 && val <= 255) return val;
     return 0;
 }
 
