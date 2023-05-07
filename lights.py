ad096f9737368dac52f679edfd28be858ccef6d0
Michael Finnessey <mrfinnessey@gmail.com>
2023-05-06 23:16:29 -0700

    Revert "refactor to tuple"
    This reverts commit c4d1ac076144bf478c92414fc7470d7c356c2329.

--- lights.py
+++ lights.py
@@ -108,11 +108,11 @@ def two_color_cycle(strip, color_1, color_2, bpm):
     # back out color values (to avoid overcomplicating function signature or
     # integrating parsing too closely) (also bithacking is cool and the
     # performance doesn't really matter)
-    c1_values = (color_1 >> 16 & bitmask, color_1 >> 8 & bitmask,
-                 color_1 & bitmask)
-    c2_values = (color_2 >> 16 & bitmask, color_2 >> 8 & bitmask,
-                 color_2 & bitmask)
-    step_values = tuple(map(operator.sub, c1_values, c2_values))
+    c1_values = [color_1 >> 16 & bitmask, color_1 >> 8 & bitmask,
+                 color_1 & bitmask]
+    c2_values = [color_2 >> 16 & bitmask, color_2 >> 8 & bitmask,
+                 color_2 & bitmask]
+    step_values = list(map(operator.sub, c1_values, c2_values))
     cur_values = c1_values
 
     # state information about current color
@@ -137,9 +137,9 @@ def two_color_cycle(strip, color_1, color_2, bpm):
 
         # update color based on shift direction
         if shift_towards_color_2:
-            cur_values = tuple(map(operator.add, cur_values, step_values))
+            cur_values = list(map(operator.add, cur_values, step_values))
         else:
-            cur_values = tuple(map(operator.sub, cur_values, step_values))
+            cur_values = list(map(operator.sub, cur_values, step_values))
 
 
 def bpm_pulse(strip, color, bpm):
